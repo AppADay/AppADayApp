0b87e99298d6e5fd738be6dd2245a71fe7e6b381
Andre Kovac <me@andrekovac.com>
2017-09-16 22:49:56 +0200

    Add blacklist filter

--- src/CameraView.js
+++ src/CameraView.js
@@ -1,8 +1,15 @@
 import React, { Component } from 'react';
-import { StyleSheet, Text, View, Platform } from 'react-native';
+import {
+  StyleSheet,
+  Text,
+  View,
+  Platform,
+  TouchableOpacity,
+} from 'react-native';
 import Emoji from 'react-native-emoji';
 import Camera from 'react-native-camera';
 import fs from 'react-native-fs';
+import idx from 'idx';
 
 import { VISION_API_KEY as API_KEY } from './constants';
 
@@ -37,7 +44,7 @@ const analyzeImage = base64Image => {
               features: [
                 {
                   type: 'LABEL_DETECTION',
-                  maxResults: 3,
+                  maxResults: 5,
                 },
               ],
             },
@@ -48,15 +55,33 @@ const analyzeImage = base64Image => {
     .then(response => response.json());
 };
 
+const blackList = [
+  'produce',
+  'fruit',
+  'food',
+  'local food',
+  'still life',
+  'cooking plantain',
+  'yuzu',
+];
+
+const filterResults = response => {
+  const results = idx(response, _ => _.responses[0].labelAnnotations);
+  return results.filter(
+    result => !blackList.includes(result.description.toLowerCase()),
+  );
+};
+
 export default class CameraView extends Component {
   takePicture = () => {
     const options = {};
     //options.location = ...
     this.camera
-      .capture({ metadata: options, jpegQuality: 80 })
+      .capture({ metadata: options, jpegQuality: 95 })
       .then(data => retrieveBase64ImageFromStorage(data.path))
       .then(image => analyzeImage(image))
-      .then(response => console.log({ response }))
+      .then(response => filterResults(response))
+      .then(filteredResults => console.log({ filteredResults }))
       .catch(err => console.error(err));
   };
 
@@ -71,9 +96,11 @@ export default class CameraView extends Component {
           aspect={Camera.constants.Aspect.fill}
           captureQuality={Camera.constants.CaptureQuality.low}
         >
-          <Text style={styles.capture} onPress={this.takePicture}>
-            <Emoji name="pineapple" />
-          </Text>
+          <TouchableOpacity onPress={this.takePicture}>
+            <Text style={styles.capture}>
+              <Emoji name="pineapple" />
+            </Text>
+          </TouchableOpacity>
         </Camera>
       </View>
     );
