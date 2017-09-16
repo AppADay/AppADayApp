f71eb1767281e8530e38613e9802c623bac3ebdc
Andre Kovac <me@andrekovac.com>
2017-09-16 22:23:18 +0200

    Get image recognition working again

--- src/CameraView.js
+++ src/CameraView.js
@@ -1,15 +1,62 @@
 import React, { Component } from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Platform } from 'react-native';
 import Emoji from 'react-native-emoji';
 import Camera from 'react-native-camera';
+import fs from 'react-native-fs';
+
+import { VISION_API_KEY as API_KEY } from './constants';
+
+const getRealImagePath = path => {
+  if (Platform.OS === 'ios') return path;
+
+  const matches = path.match(/^file:\/{2}(.+\.jpg)$/);
+  return matches[1] || path;
+};
+
+const retrieveBase64ImageFromStorage = imagePath => {
+  const path = getRealImagePath(imagePath);
+
+  if (path && fs.exists(path)) {
+    return Promise.resolve().then(() => fs.readFile(path, 'base64'));
+  }
+  return null;
+};
+
+const analyzeImage = base64Image => {
+  if (!base64Image) return null;
+  return Promise.resolve()
+    .then(() =>
+      fetch(`https://vision.googleapis.com/v1/images:annotate?key=${API_KEY}`, {
+        method: 'POST',
+        body: JSON.stringify({
+          requests: [
+            {
+              image: {
+                content: base64Image,
+              },
+              features: [
+                {
+                  type: 'LABEL_DETECTION',
+                  maxResults: 3,
+                },
+              ],
+            },
+          ],
+        }),
+      }),
+    )
+    .then(response => response.json());
+};
 
 export default class CameraView extends Component {
   takePicture = () => {
     const options = {};
     //options.location = ...
     this.camera
-      .capture({ metadata: options })
-      .then(data => console.log(data))
+      .capture({ metadata: options, jpegQuality: 80 })
+      .then(data => retrieveBase64ImageFromStorage(data.path))
+      .then(image => analyzeImage(image))
+      .then(response => console.log({ response }))
       .catch(err => console.error(err));
   };
 
@@ -22,6 +69,7 @@ export default class CameraView extends Component {
           }}
           style={styles.preview}
           aspect={Camera.constants.Aspect.fill}
+          captureQuality={Camera.constants.CaptureQuality.low}
         >
           <Text style={styles.capture} onPress={this.takePicture}>
             <Emoji name="pineapple" />
